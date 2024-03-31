6a29b2578d8e11547c4772ae64c6caf17e024f01
charliehyin <charliehyin@gmail.com>
2024-03-31 14:50:18 -0500

    adding firebase config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'firebase/database';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHMX3LqauP2z1mdng1xgaeHRf5qjAA9bY",
+  authDomain: "purple-hours.firebaseapp.com",
+  databaseURL: "https://purple-hours-default-rtdb.firebaseio.com",
+  projectId: "purple-hours",
+  storageBucket: "purple-hours.appspot.com",
+  messagingSenderId: "289069179177",
+  appId: "1:289069179177:web:91b16f6e4da77b7f611738"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const App = () => {
   const [count, setCount] = useState(0);
