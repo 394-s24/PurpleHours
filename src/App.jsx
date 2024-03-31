f4bb007e7c1267c1ffcf2f6308b555b20c2899e5
Daniel <daniel.albert.liu@gmail.com>
2024-03-31 16:52:34 -0500

    Worked on queue

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css';
 import Queue from './components/Queue.jsx';
 import 'firebase/database';
-import { initializeApp } from "firebase/app";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { initializeApp } from 'firebase/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import {} from 'react-bootstrap';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCHMX3LqauP2z1mdng1xgaeHRf5qjAA9bY",
-  authDomain: "purple-hours.firebaseapp.com",
-  databaseURL: "https://purple-hours-default-rtdb.firebaseio.com",
-  projectId: "purple-hours",
-  storageBucket: "purple-hours.appspot.com",
-  messagingSenderId: "289069179177",
-  appId: "1:289069179177:web:91b16f6e4da77b7f611738"
+  apiKey: 'AIzaSyCHMX3LqauP2z1mdng1xgaeHRf5qjAA9bY',
+  authDomain: 'purple-hours.firebaseapp.com',
+  databaseURL: 'https://purple-hours-default-rtdb.firebaseio.com',
+  projectId: 'purple-hours',
+  storageBucket: 'purple-hours.appspot.com',
+  messagingSenderId: '289069179177',
+  appId: '1:289069179177:web:91b16f6e4da77b7f611738',
 };
 
 // Initialize Firebase
@@ -21,6 +21,8 @@ const app = initializeApp(firebaseConfig);
 
 const queueData = [
   { name: 'Ella', issue: "My code doesn't work", time: '1:27 PM' },
+  { name: 'James', issue: 'I found a bug', time: '1:30 PM' },
+  { name: 'Anna', issue: 'Need help with an error', time: '1:45 PM' },
 ];
 
 const App = () => {
--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -1,47 +1,44 @@
-// import React from 'react';
-// import Group from './Group';
-// import './Queue.css';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import {ListGroup, Button } from 'react-bootstrap';
-
-// // The Queue component
-// const Queue = ({ queue }) => {
-//     return (
-//       <div className="queue">
-//         {queue.map((group, index) => (
-//           <Group
-//             key={index}
-//             name={group.name}
-//             issue={group.issue}
-//             time={group.time}
-//           />
-//         ))}
-//       </div>
-//     );
-// };
-
-import React from 'react';
+import Group from './Group';
+import './Queue.css';
 import { ListGroup, Button } from 'react-bootstrap';
-import './Queue.css'; // Your custom CSS
 
-const Queue = () => (
-  <div className="queue-container">
-    <h2>Dave's 211 Queue</h2>
-    <ListGroup>
-      {/* Currently Helping */}
-      <ListGroup.Item className="currently-helping">
-        Ella - My code doesn't work
-        <span className="time">1:27 PM</span>
-      </ListGroup.Item>
-      
-      {/* Upcoming */}
-      <ListGroup.Item variant="secondary">Steven - Debugging - BFS <span className="time">1:32 PM</span></ListGroup.Item>
-      <ListGroup.Item variant="info">Gus - Conceptual - Don't know how to start assign. <span className="time">1:44 PM</span></ListGroup.Item>
-      {/* ... other items */}
-    </ListGroup>
-    <Button variant="danger" className="end-session-btn">End Session</Button>
-  </div>
-);
-
+// The Queue component
+const Queue = ({ queue }) => {
+  const first = queue ? queue[0] : null;
+  const rest = queue.slice(1);
+  console.log(rest);
+  return (
+    <div className="queue">
+      <div>
+        <h2>Currently helping</h2>
+        <Group name={first.name} issue={first.issue} time={first.time} />
+      </div>
+      <div>
+        <h2>Upcoming</h2>
+        {/* <ListGroup>
+          {rest.map((group, index) => {
+            console.log(group);
+            <ListGroup.Item>
+              <Group
+                key={index}
+                name={group.name}
+                issue={group.issue}
+                time={group.time}
+              />
+            </ListGroup.Item>
+          })}
+        </ListGroup> */}
+        {rest.map((group, index) => {
+          <Group
+            key={index}
+            name={group.name}
+            issue={group.issue}
+            time={group.time}
+          />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Queue;
