2707ba0ff3caf7b6d28195b6f056819c16d304f6
Daniel <daniel.albert.liu@gmail.com>
2024-04-12 20:23:04 -0500

    Updated queue timestamps

--- src/components/NewGroup.jsx
+++ src/components/NewGroup.jsx
@@ -3,7 +3,15 @@ import { Button, Modal, Form } from 'react-bootstrap';
 
 import { addToGroup, createNewGroup } from '../DatabaseFuncs.mjs';
 
-const NewGroup = ({ joinedGroupId, nameID, setJoinedGroupId, setNameID, onFormSubmit, studentData, ...props }) => {
+const NewGroup = ({
+  joinedGroupId,
+  nameID,
+  setJoinedGroupId,
+  setNameID,
+  onFormSubmit,
+  studentData,
+  ...props
+}) => {
   const [helpType, setHelpType] = useState('');
   const [helpDescription, setHelpDescription] = useState('');
   const [validated, setValidated] = useState(false);
@@ -15,33 +23,42 @@ const NewGroup = ({ joinedGroupId, nameID, setJoinedGroupId, setNameID, onFormSu
   const handleHelpDescriptionChange = (e) => {
     setHelpDescription(e.target.value);
   };
-  
+
   const handleSubmit = async (event) => {
     const form = event.currentTarget;
-    
+
     event.preventDefault();
     event.stopPropagation();
-    
-    if (form.checkValidity()){
+
+    if (form.checkValidity()) {
       const groupsData = {
-      issue: `${helpType}: ${helpDescription}`,
-      time : Math.floor(Date.now() / 1000),
-      done : false,
-      public : true
-    };
-     // Pass the data to an external function
-     let groupID = await createNewGroup(studentData.course, studentData.session, groupsData);
-     console.log(groupID)
-     let id = await addToGroup(studentData.course, studentData.session, studentData.name, groupID);
-     props.onHide();
-     // console.log(groupID, nameID);
-     // onFormSubmit(groupID, nameID);
-     setJoinedGroupId([...joinedGroupId, groupID]);
-     setNameID([...nameID, id]);
-  }
-  
+        issue: `${helpType}: ${helpDescription}`,
+        time: Math.floor(Date.now() / 1000),
+        done: false,
+        public: true,
+      };
+      // Pass the data to an external function
+      let groupID = await createNewGroup(
+        studentData.course,
+        studentData.session,
+        groupsData
+      );
+      console.log(groupID);
+      let id = await addToGroup(
+        studentData.course,
+        studentData.session,
+        studentData.name,
+        groupID
+      );
+      props.onHide();
+      // console.log(groupID, nameID);
+      // onFormSubmit(groupID, nameID);
+      setJoinedGroupId([...joinedGroupId, groupID]);
+      setNameID([...nameID, id]);
+    }
+
     setValidated(true);
-  }
+  };
 
   return (
     <Modal
@@ -51,65 +68,60 @@ const NewGroup = ({ joinedGroupId, nameID, setJoinedGroupId, setNameID, onFormSu
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          New Group
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">New Group</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-          <Form noValidate validated={validated}  onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="helpType">
-              <Form.Label>What type of help do you need?</Form.Label>
-              <div key={`inline-radio`} className="mb-3">
-                <Form.Check
-                  inline
-                  label="Conceptual"
-                  name="helpType"
-                  type="radio"
-                  id="Conceptual"
-                  onChange={handleHelpTypeChange}
-                />
-                <Form.Check
-                  inline
-                  label="Debugging"
-                  name="helpType"
-                  type="radio"
-                  id="Debugging"
-                  onChange={handleHelpTypeChange}
-                />
-                <Form.Check
-                  inline
-                  label="Other"
-                  name="helpType"
-                  type="radio"
-                  id="Other"
-                  onChange={handleHelpTypeChange}
-                />
-              </div>
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="helpDescription"
-            >
-              <Form.Label>What do you need help with?</Form.Label>
-              <Form.Control required
-                as="textarea"
-                rows={3}
-                name="helpDescription"
-                onChange={handleHelpDescriptionChange}
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="helpType">
+            <Form.Label>What type of help do you need?</Form.Label>
+            <div key={`inline-radio`} className="mb-3">
+              <Form.Check
+                inline
+                label="Conceptual"
+                name="helpType"
+                type="radio"
+                id="Conceptual"
+                onChange={handleHelpTypeChange}
               />
-               <Form.Control.Feedback type="invalid">
-                 Please enter a detailed description of the problem
-                </Form.Control.Feedback>
-            </Form.Group>
-            <Button type = "submit" variant="dark" >
+              <Form.Check
+                inline
+                label="Debugging"
+                name="helpType"
+                type="radio"
+                id="Debugging"
+                onChange={handleHelpTypeChange}
+              />
+              <Form.Check
+                inline
+                label="Other"
+                name="helpType"
+                type="radio"
+                id="Other"
+                onChange={handleHelpTypeChange}
+              />
+            </div>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="helpDescription">
+            <Form.Label>What do you need help with?</Form.Label>
+            <Form.Control
+              required
+              as="textarea"
+              rows={3}
+              name="helpDescription"
+              onChange={handleHelpDescriptionChange}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a detailed description of the problem
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button type="submit" variant="dark">
             Submit
           </Button>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-        </Modal.Footer>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
     </Modal>
   );
-}
+};
 
 export default NewGroup;
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -5,16 +5,20 @@ import './Student.css';
 import 'firebase/database';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
-import { createNewGroup, addToGroup, removeFromGroup} from '../DatabaseFuncs.mjs';
+import {
+  createNewGroup,
+  addToGroup,
+  removeFromGroup,
+} from '../DatabaseFuncs.mjs';
 import { set } from 'firebase/database';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-const Student = ({queue, studentData}) => {
+const Student = ({ queue, studentData }) => {
   const [refinedQueue, setRefinedQueue] = useState([]);
   const [nameID, setNameID] = useState([]);
   const [modalShow, setModalShow] = useState(false); // State to track modal
-  const [clientJoined, setClientJoined] = useState(false) // State to track if client clicked a join button
+  const [clientJoined, setClientJoined] = useState(false); // State to track if client clicked a join button
   const [joinedGroupId, setJoinedGroupId] = useState([]); // State to track the group id of a group the client joined
 
   const renderQueue = () => {
@@ -23,21 +27,30 @@ const Student = ({queue, studentData}) => {
     if (queue) {
       // Format queue data
       const formattedQueue = Object.values(queue).map((item) => {
-        // Convert unix time to readable time
+        // Convert unix time to readable time with specific format: "4:10PM, 3/27"
         const unixTimestamp = item.time;
         const date = new Date(unixTimestamp * 1000);
-        const hours = date.getHours();
-        const minutes = "0" + date.getMinutes();
-        const formattedTime = isNaN(date.getTime()) ? "Invalid Time" : `${hours}:${minutes.substr(-2)}`;
+        let hours = date.getHours();
+        const minutes = '0' + date.getMinutes();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        const month = date.getMonth() + 1; // Months are zero indexed, so add 1
+        const day = date.getDate();
+        const formattedTime = isNaN(date.getTime())
+          ? 'Invalid Date'
+          : `${hours}:${minutes.substr(-2)}${ampm}, ${month}/${day}`;
 
         // Convert list of names to string
-        const namesObjects = item["names"];
-        let namesArray = ["No members"];
+        const namesObjects = item['names'];
+        let namesArray = ['No members'];
         if (namesObjects) {
-          namesArray = Object.values(namesObjects).map((obj) => {return obj["name"]});
+          namesArray = Object.values(namesObjects).map((obj) => {
+            return obj['name'];
+          });
         }
-        const namesString = namesArray.join(", "); 
-        
+        const namesString = namesArray.join(', ');
+
         // Return a new object with formatted time and names
         return {
           ...item,
@@ -57,14 +70,26 @@ const Student = ({queue, studentData}) => {
     setClientJoined(true);
     // setJoinedGroupId(joinedGroupId.concat([groupID]));
     setJoinedGroupId([...joinedGroupId, groupID]);
-    let id = await addToGroup(studentData.course, studentData.session, studentData.name, groupID)
+    let id = await addToGroup(
+      studentData.course,
+      studentData.session,
+      studentData.name,
+      groupID
+    );
     setNameID([...nameID, id]);
   };
 
   const handleLeaveQueue = (studentData, groupID) => {
     setClientJoined(false);
-    setJoinedGroupId(joinedGroupId.toSpliced(joinedGroupId.indexOf(groupID), 1));
-    removeFromGroup(studentData.course, studentData.session, nameID[joinedGroupId.indexOf(groupID)], groupID);
+    setJoinedGroupId(
+      joinedGroupId.toSpliced(joinedGroupId.indexOf(groupID), 1)
+    );
+    removeFromGroup(
+      studentData.course,
+      studentData.session,
+      nameID[joinedGroupId.indexOf(groupID)],
+      groupID
+    );
     setNameID(nameID.toSpliced(joinedGroupId.indexOf(groupID), 1));
   };
 
@@ -74,27 +99,32 @@ const Student = ({queue, studentData}) => {
     // setJoinedGroupId([...joinedGroupId, groupID]);
     // let id = await addToGroup(studentData.course, studentData.session, studentData.name, groupID)
     // setNameID([...nameID, id]);
-    console.log("hello");
-  }
+    console.log('hello');
+  };
 
   const navigate = useNavigate();
   const handleBack = () => {
-    navigate('/')
+    navigate('/');
   };
   return (
     <div className="student_view">
-      <Button variant="dark" onClick={() => handleBack()}>Go Back</Button>
+      <Button variant="dark" onClick={() => handleBack()}>
+        Go Back
+      </Button>
       <div className="queue">
         <StudentQueue
           queue={refinedQueue}
           studentData={studentData}
           clientJoined={clientJoined}
-          joinedID = {joinedGroupId}
+          joinedID={joinedGroupId}
           joinQueue={handleJoinQueue}
-          leaveQueue={handleLeaveQueue} />
+          leaveQueue={handleLeaveQueue}
+        />
       </div>
       <div className="new">
-        <Button variant="dark" onClick={() => setModalShow(true)}>New Group</Button>
+        <Button variant="dark" onClick={() => setModalShow(true)}>
+          New Group
+        </Button>
         {/* Bootstrap modal */}
         <NewGroup
           studentData={studentData}
@@ -108,7 +138,6 @@ const Student = ({queue, studentData}) => {
         />
       </div>
     </div>
-
   );
 };
 
