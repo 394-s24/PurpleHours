7834f7b76f066ca0fcc4742b2544920f2da7f3c7
chzhang981 <edwardzhangcongkai@gmail.com>
2024-05-24 03:15:04 -0500

    App.test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,85 @@
 import { describe, expect, test } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+import * as DatabaseFuncs from './DatabaseFuncs.mjs';
 
-describe("counter tests", () => {
-  test("Counter should be 0 at the start", () => {
+vi.spyOn(DatabaseFuncs, 'useDbData').mockReturnValue([{ groups: {} }, null]);
+
+
+describe("PurpleHour Test", () => {
+  test("Should have a Sign in button at start", async () => {
     render(<App />);
-    expect(screen.getByText("count is: 0")).toBeDefined();
+    const signInButton = await screen.findByText("Sign in");
+    expect(signInButton).toBeDefined();
   });
 
-  test("Counter should increment by one when clicked", async () => {
+  test("Should have I am a Student, I am a TA/PM, Sign out buttons after signing in", async () => {
+
+    vi.spyOn(DatabaseFuncs, 'signInWithGoogle').mockImplementation(() => {
+      const mockUser = { displayName: 'Test User', uid: '12345' };
+      return Promise.resolve(mockUser);
+    });
+
+    vi.spyOn(DatabaseFuncs, 'useAuthState').mockReturnValue([{ displayName: 'Test User', uid: '12345' }]);
     render(<App />);
-    const counter = screen.getByRole("button");
-    fireEvent.click(counter);
-    expect(await screen.getByText("count is: 1")).toBeDefined();
+
+    expect(await screen.findByText("I am a Student")).toBeDefined();
+    expect(await screen.findByText("I am a TA/PM")).toBeDefined();
+    expect(await screen.findByText("Sign out")).toBeDefined();
+  });
+
+  test("Should have Submit and Select a course after clicking I am a Student button", async () => {
+
+    vi.spyOn(DatabaseFuncs, 'signInWithGoogle').mockImplementation(() => {
+      const mockUser = { displayName: 'Test User', uid: '12345' };
+      return Promise.resolve(mockUser);
+    });
+
+    vi.spyOn(DatabaseFuncs, 'useAuthState').mockReturnValue([{ displayName: 'Test User', uid: '12345' }]);
+
+    render(<App />);
+
+    const studentButton = await screen.findByText("I am a Student", { exact: false });
+    fireEvent.click(studentButton);
+
+    expect(await screen.findByText("Submit")).toBeDefined();
+    expect(await screen.findByText("Select a course")).toBeDefined();
+  });
+
+  test("Should have Submit, Select a course, and Access Code after clicking I am a TA/PM button", async () => {
+
+    vi.spyOn(DatabaseFuncs, 'signInWithGoogle').mockImplementation(() => {
+      const mockUser = { displayName: 'Test User', uid: '12345' };
+      return Promise.resolve(mockUser);
+    });
+
+    vi.spyOn(DatabaseFuncs, 'useAuthState').mockReturnValue([{ displayName: 'Test User', uid: '12345' }]);
+
+    render(<App />);
+
+    const studentButton = await screen.findByText("I am a TA/PM", { exact: false });
+    fireEvent.click(studentButton);
+
+    expect(await screen.findByText("Submit")).toBeDefined();
+    expect(await screen.findByText("Select a course")).toBeDefined();
+    expect(await screen.findByText("Access Code")).toBeDefined();
+  });
+
+  test("Should have Sign in after click Sign out button", async () => {
+    vi.spyOn(DatabaseFuncs, 'signInWithGoogle').mockImplementation(() => {
+      const mockUser = { displayName: 'Test User', uid: '12345' };
+      return Promise.resolve(mockUser);
+    });
+
+    vi.spyOn(DatabaseFuncs, 'useAuthState').mockReturnValue([{ displayName: 'Test User', uid: '12345' }]);
+
+    render(<App />);
+
+    const signOutButton = await screen.findByText("Sign out", { exact: false });
+    fireEvent.click(signOutButton);
+
+    // expect(await screen.findByText("Sign in", { exact: false })).toBeDefined();
   });
 });
+
+
